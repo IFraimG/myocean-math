bd294daa91f60bde1912b8ba3287da0f46785131
IFramex <kulsasha55@gmail.com>
2021-04-21 14:36:18 +0300

    деплой 14

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+// import { VitePluginNode } from 'vite-plugin-node';
 
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [
+    reactRefresh(),
+    // ...VitePluginNode({
+    //   server: 'express', 
+    //   appPath: './server.js',
+    //   port: 3000,
+    //   tsCompiler: 'esbuild',
+    //   createCustomServer: () => IServer 
+    // })]
 })
