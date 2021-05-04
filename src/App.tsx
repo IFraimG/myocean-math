4e67207ea6d495694b6a963e14103a094120994d
IFramex <kulsasha55@gmail.com>
2021-05-04 18:28:45 +0300

    деплой 20

--- src/App.tsx
+++ src/App.tsx
@@ -11,17 +11,6 @@ import HomePage from './pages/Home'
 import LevelPage from './pages/Level'
 import ProfilePage from './pages/Profile'
 
-// const pages = import.meta.globEager('./pages/*.jsx')
-
-// const routes = Object.keys(pages).map((path) => {
-//   const name = path.match(/\.\/pages\/(.*)\.jsx$/)[1]
-//   return {
-//     name,
-//     path: name === 'Home' ? '/' : `/${name.toLowerCase()}`,
-//     component: pages[path].default
-//   }
-// })
-
 function App() {
   const dispatch = useDispatch()
   const { isLoader } = useSelector((state: AppStateType) => state.AuthReducer)
@@ -34,7 +23,7 @@ function App() {
   }, [])
 
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       { !isLoader ? <Header /> : "" }
       <Switch>
         <Route exact path="/">
